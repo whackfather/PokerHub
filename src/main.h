a7c1337aef848cf9eae4a89992a3c15840956f8b
Roman Rodriguez <rodriguezr6216@gmail.com>
2025-03-06 13:42:34 -0700

    added comments and updated function declarations

--- src/main.h
+++ src/main.h
@@ -1,13 +1,20 @@
+// PokerHub main header file
+// Written by Roman Rodriguez
+
+// Include statements
 #include <vector>
 #include <string>
 using namespace std;
 
+// Function declarations
+vector<vector<string>> getNightInfo(string, vector<vector<string>>);
 vector<vector<string>> readData(string);
 void writeData();
 vector<string> split(const string&, char);
 float roundUp(float, int);
 float roundDown(float, int);
 
+// Game information struct
 typedef struct {
     string date;
     int noOfPlayers;
@@ -15,6 +22,7 @@ typedef struct {
     float dealerTotal;
 } gameInfo;
 
+// Player information struct
 typedef struct {
     string name;
     float buyIn;
