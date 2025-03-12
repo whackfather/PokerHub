0cc68fbed40f0b6565f83318e6346d5f6bfadf2f
Roman Rodriguez <rodriguezr6216@gmail.com>
2025-03-12 15:28:31 -0600

    automatic updates

--- build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/ui_mainwindow.h
+++ build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -10,9 +10,13 @@
 #define UI_MAINWINDOW_H
 
 #include <QtCore/QVariant>
+#include <QtGui/QAction>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QProgressBar>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
 
@@ -22,7 +26,13 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
+    QProgressBar *progressBar;
+    QLabel *label;
     QMenuBar *menubar;
+    QMenu *menuFile;
+    QMenu *menuEdit;
+    QMenu *menuView;
+    QMenu *menuHelp;
     QStatusBar *statusbar;
 
     void setupUi(QMainWindow *MainWindow)
@@ -32,14 +42,35 @@ public:
         MainWindow->resize(800, 600);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName("centralwidget");
+        progressBar = new QProgressBar(centralwidget);
+        progressBar->setObjectName("progressBar");
+        progressBar->setGeometry(QRect(350, 280, 118, 23));
+        progressBar->setValue(2);
+        label = new QLabel(centralwidget);
+        label->setObjectName("label");
+        label->setGeometry(QRect(350, 260, 111, 16));
+        label->setAlignment(Qt::AlignmentFlag::AlignCenter);
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName("menubar");
         menubar->setGeometry(QRect(0, 0, 800, 21));
+        menuFile = new QMenu(menubar);
+        menuFile->setObjectName("menuFile");
+        menuEdit = new QMenu(menubar);
+        menuEdit->setObjectName("menuEdit");
+        menuView = new QMenu(menubar);
+        menuView->setObjectName("menuView");
+        menuHelp = new QMenu(menubar);
+        menuHelp->setObjectName("menuHelp");
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName("statusbar");
         MainWindow->setStatusBar(statusbar);
+
+        menubar->addAction(menuFile->menuAction());
+        menubar->addAction(menuEdit->menuAction());
+        menubar->addAction(menuView->menuAction());
+        menubar->addAction(menuHelp->menuAction());
 
         retranslateUi(MainWindow);
 
@@ -49,6 +80,11 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "Loading...", nullptr));
+        menuFile->setTitle(QCoreApplication::translate("MainWindow", "File", nullptr));
+        menuEdit->setTitle(QCoreApplication::translate("MainWindow", "Edit", nullptr));
+        menuView->setTitle(QCoreApplication::translate("MainWindow", "View", nullptr));
+        menuHelp->setTitle(QCoreApplication::translate("MainWindow", "Help", nullptr));
     } // retranslateUi
 
 };
