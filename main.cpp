4b638750deac204549f3593219a04af59ab838bc
Roman Rodriguez <rodriguezr6216@gmail.com>
2025-03-15 21:46:24 -0600

    removed split function, moved writeData function to be the function of a button press

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 // Written by Roman Rodriguez
 
 // Include statements
-#include <iostream>
 #include <cmath>
 #include <cstdio>
 #include <vector>
@@ -83,73 +82,6 @@ vector<vector<string>> readData(string filePath) {
 
     file.close();
     return data;
-}
-
-// Write new data to csv file
-void writeData() {
-    return; // WARNING: Calling this function in its current state will depricate the csv file.
-    // Remove return statement when the code below becomes functional.
-    gameInfo game;
-    FILE *data;
-    data = fopen("data.csv", "a");
-
-    cout << "Date of play (MMDDYY): "; cin >> game.date;
-    cout << "Number of players: "; cin >> game.noOfPlayers;
-    cout << "Dealer tip (%): "; cin >> game.dealerTipPerc;
-    game.dealerTipPerc /= 100.0f;
-
-    fprintf(data, "%s,%d,0,0,0,0\n", game.date.c_str(), game.noOfPlayers);
-
-    vector<playerInfo> players(game.noOfPlayers);
-
-    for (int i = 0; i < game.noOfPlayers; i++) {
-        printf("Player %d\n", i + 1);
-        cout << "Name: "; cin >> players[i].name;
-        cout << "Buy in: "; cin >> players[i].buyIn;
-        cout << "Gross winnings: "; cin >> players[i].grossWin;
-        cout << "Custom tip? (y/n): "; cin >> players[i].diffTip;
-
-        if (players[i].diffTip == 'y') {
-            cout << "Custom tip: "; cin >> players[i].tip;
-        } else {
-            players[i].tip = roundDown(game.dealerTipPerc * players[i].grossWin, 2);
-        }
-        players[i].postTip = players[i].grossWin - players[i].tip;
-        players[i].netWin = players[i].postTip - players[i].buyIn;
-        fprintf(data, "%s,%.2f,%.2f,%.2f,%.2f,%.2f\n",
-                players[i].name.c_str(),
-                players[i].buyIn,
-                players[i].grossWin,
-                players[i].tip,
-                players[i].postTip,
-                players[i].netWin);
-        game.totalBuyIn += players[i].buyIn;
-        game.totalGrossWin += players[i].grossWin;
-        game.totalTip += players[i].tip;
-        game.totalPostTip += players[i].postTip;
-        game.totalNetWin += players[i].netWin;
-    }
-
-    fprintf(data, "TOTAL,%.2f,%.2f,%.2f,%.2f,%.2f\n",
-            game.totalBuyIn,
-            game.totalGrossWin,
-            game.totalTip,
-            game.totalPostTip,
-            game.totalNetWin);
-    fprintf(data, "END,0,0,0,0,0\n");
-
-    fclose(data);
-}
-
-// Split a string into a vector of strings given a delimiter
-vector<string> split(const string& str, char delimiter) {
-    vector<string> cells;
-    stringstream ss(str);
-    string cell;
-    while (getline(ss, cell, delimiter)) {
-        cells.push_back(cell);
-    }
-    return cells;
 }
 
 // Round up a number to a given number of decimals
