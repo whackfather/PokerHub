ae131a7b08346494ca36c90488f7da619e90c688
Roman Rodriguez <rodriguezr6216@gmail.com>
2025-03-15 21:46:57 -0600

    removed unnecessary custom datatypes and split function declaration

--- main.h
+++ main.h
@@ -10,29 +10,6 @@
 #include <string>
 using namespace std;
 
-// Game information struct
-typedef struct gameInfo {
-    string date;
-    int noOfPlayers;
-    float dealerTipPerc;
-    float totalBuyIn = 0;
-    float totalGrossWin = 0;
-    float totalTip = 0;
-    float totalPostTip = 0;
-    float totalNetWin = 0;
-} gameInfo;
-
-// Player information struct
-typedef struct playerInfo {
-    string name;
-    float buyIn;
-    float grossWin;
-    char diffTip;
-    float tip;
-    float postTip;
-    float netWin;
-} playerInfo;
-
 // Columns type definition
 typedef enum columns {
     NAME,
@@ -49,7 +26,6 @@ vector<vector<string>> getPlayerInfo(string, vector<vector<string>>);
 vector<vector<string>> getNightInfo(string, vector<vector<string>>);
 vector<vector<string>> readData(string);
 void writeData();
-vector<string> split(const string&, char);
 float roundUp(float, int);
 float roundDown(float, int);
 
